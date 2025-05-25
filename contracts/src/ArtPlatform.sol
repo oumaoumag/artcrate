a50cc7fffed1a0458947cfc68018e5144df61ca9
oumaoumag <ouma.godwin10@gmail.com>
2025-05-25 23:19:09 +0300

    feat: import OpenZeppelin contracts for ERC20, ERC721, Ownable, and ReentrancyGuard in ArtPlatform

--- contracts/src/ArtPlatform.sol
+++ contracts/src/ArtPlatform.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 contract ArtPlatform  is ERC20, ERC721UIStorage, Owner, ReentrancyGuard {
     
 }
