d26c13d1a296f9c16790bb483a60ac36b4f08b24
oumaoumag <ouma.godwin10@gmail.com>
2025-05-25 22:20:42 +0300

    feat: implement Web3Provider for Ethereum account and contract management

--- frontend/src/context/Web3Context.js
+++ frontend/src/context/Web3Context.js
@@ -1,2 +1,12 @@
 import React, { createContext, useState } from 'react';
 import { ethers } from 'ethers';
+
+export const Web3Context = createContext();
+
+export const Web3Provider = ({ children }) => {
+    const [account, setAcount] = useState(' ');
+    const [contract, setContract] = useState(null);
+    const [provider, setProvider] = useState(null);
+
+
+}
