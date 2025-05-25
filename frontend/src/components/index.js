95f83926c1511634c449af452e159371b963e1d9
oumaoumag <ouma.godwin10@gmail.com>
2025-05-26 02:40:19 +0300

    feat: add index.js to export all components for easier importing

---
+++ frontend/src/components/index.js
@@ -0,0 +1,9 @@
+// Export all components for easier importing
+export { default as Header } from './Header';
+export { default as TokenBalance } from './TokenBalance';
+export { default as MintForm } from './MintForm';
+export { default as Gallery } from './Gallery';
+export { default as MintingLog } from './MintingLog';
+export { default as StatsOverview } from './StatsOverview';
+export { default as Navigation } from './Navigation';
+export { default as DemoContent } from './DemoContent';
