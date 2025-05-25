c2a32ede43d1b1bb5e0aa5a76b480a1330f850fa
oumaoumag <ouma.godwin10@gmail.com>
2025-05-26 01:30:06 +0300

    feat: add Tailwind CSS configuration for styling and theming

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          900: '#581c87',
+          800: '#7c3aed',
+        },
+        orange: {
+          900: '#9a3412',
+          800: '#ea580c',
+          500: '#f97316',
+          400: '#fb923c',
+          300: '#fdba74',
+          200: '#fed7aa',
+        },
+        yellow: {
+          500: '#eab308',
+          400: '#facc15',
+          300: '#fde047',
+        },
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
