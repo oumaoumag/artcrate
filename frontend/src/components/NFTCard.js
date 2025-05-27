1cd36151b3bc0bd0925f99caecb928e03db27036
oumaoumag <ouma.godwin10@gmail.com>
2025-05-27 17:42:36 +0300

    feat: Add NFTCard component with initial imports for NFT functionality

---
+++ frontend/src/components/NFTCard.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { useWeb3 } from '../context/Web3Context';
+import { Image, Send } from 'lucide-react';
+import TransferNFTModal from './TransferNFTModal';
