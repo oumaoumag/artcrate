aed1b73bab63350b883d4171bff22013cc3a5be1
oumaoumag <ouma.godwin10@gmail.com>
2025-06-01 04:51:15 +0300

    fix: Correct import statement for React in NFTManager test file

--- frontend/src/components/__tests__/NFTManager.test.js
+++ frontend/src/components/__tests__/NFTManager.test.js
@@ -1,4 +1,4 @@
-sr  import React from 'react';
+import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import NFTManager from '../NFTManager';
 import { useWeb3 } from '../../context/Web3Context';
